f552345e634b36bc833a5b86efa8dd958f36fdc9
Uros Stojkovic <uros.stojkovich@outlook.com>
2023-12-27 19:36:56 +0100

    complete courseinfo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Part = (props) => <p>{props.name} {props.exercises}</p>
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </>
+  )
+}
+
+const Total = (props) => <p>Number of exercises {props.parts.reduce((acc, elem) => acc + elem.exercises, 0)}</p>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
